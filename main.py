0887ab0a27a39cc278f4549daa3e0be0dffb2483
goldimaurya <goldi.maurya_cs19@gla.ac.in>
2021-07-20 22:24:41 +0530

    final commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pyautogui
+while True:
+    pyautogui.typewrite('Happy Birthday Dear')
+    pyautogui.press('enter')
+
+# target your curser on messager FB whom you want to send message .
+# If you want to delay time :
+# step 1: import Time
+# step 2: time.sleep(3)
